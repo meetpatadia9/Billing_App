799b917bea4bd033668ba5e22dda885aa1db46b7
IPS-030 <meetp-itpath@gmail.com>
2023-03-31 17:55:30 +0530

    Done: PartiesFragment.kt (Upload data on firebase) Done: PartyUpdateFragment.kt (Update or Delete product)

--- app/src/main/java/com/codebyzebru/myapplication/fragments/PartyUpdateFragment.kt
+++ app/src/main/java/com/codebyzebru/myapplication/fragments/PartyUpdateFragment.kt
@@ -5,9 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
 import com.codebyzebru.myapplication.R
 import com.codebyzebru.myapplication.activities.HomeActivity
+import com.codebyzebru.myapplication.dataclasses.AddPartyDataClass
 import com.codebyzebru.myapplication.dataclasses.ViewPartyDataClass
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ktx.database
@@ -35,6 +40,7 @@ class PartyUpdateFragment(private val listDataClass: ViewPartyDataClass) : Fragm
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        key = listDataClass.key
         update_partyName.setText(listDataClass.partyName)
         update_company_name.setText(listDataClass.companyName)
         update_address.setText(listDataClass.address)
@@ -47,16 +53,47 @@ class PartyUpdateFragment(private val listDataClass: ViewPartyDataClass) : Fragm
             update_rBtn_B2C.isChecked = true
         }
 
-        btnUpdateParty.setOnClickListener {
+        val radioGroup: RadioGroup = view.findViewById(R.id.update_rg_type)
+        var radioButton: RadioButton?
 
+        btnUpdateParty.setOnClickListener {
+            val selectedID = radioGroup.checkedRadioButtonId
+            radioButton = view.findViewById(selectedID)
+
+            val addParty = AddPartyDataClass(
+                partyName = update_partyName.text.toString(),
+                companyName = update_company_name.text.toString(),
+                address = update_address.text.toString(),
+                email = update_email.text.toString(),
+                contact = update_contact.text.toString(),
+                type = radioButton!!.text.toString()
+            )
+
+            val userID = FirebaseAuth.getInstance().currentUser!!.uid
+            val thisKey = database.child("Party Data").push().key.toString()
+            dbRef.getReference("Users/$userID").child("Party Data").child(thisKey).setValue(addParty)
+
+            FirebaseDatabase.getInstance().getReference("Users/$userID/Party Data").child(key).removeValue()
+                .addOnSuccessListener {
+                    requireActivity().supportFragmentManager.beginTransaction().replace(R.id.layout_home, PartiesFragment()).addToBackStack(null).commit()
+                    Toast.makeText(context, "Party Updated!!", Toast.LENGTH_SHORT).show()
+                }
         }
 
         btnDeleteParty.setOnClickListener {
-
+            val userID = FirebaseAuth.getInstance().currentUser!!.uid
+            FirebaseDatabase.getInstance().getReference("Users/$userID/Party Data").child(key).removeValue()
+                .addOnSuccessListener {
+                    requireActivity().supportFragmentManager.beginTransaction().replace(R.id.layout_home, PartiesFragment()).addToBackStack(null).commit()
+                    Toast.makeText(context, "Party Deleted!!", Toast.LENGTH_SHORT).show()
+                }
         }
 
         btn_update_party_cancel.setOnClickListener {
-
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(R.id.layout_home, PartiesFragment())
+                .addToBackStack(null)
+                .commit()
         }
     }
 
